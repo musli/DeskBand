c25dd86e168a5cd143e79b6f60d14b322c2b74e1
卢小辉 <muslilu@dingtalk.com>
2021-04-26 14:39:39 +0800

    添加窗体位置实时更新

--- src/DeskBand/DeskBand/MainWindow.xaml.cs
+++ src/DeskBand/DeskBand/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -33,38 +34,79 @@ namespace DeskBand
             MessageBox.Show("hello");
         }
 
+        IntPtr panelHandle = IntPtr.Zero;
+        IntPtr taskBarHandle = IntPtr.Zero;
+        IntPtr windowHandle = IntPtr.Zero;
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             try
             {
                 //本窗口句柄
-                var help = new WindowInteropHelper(this);
-                var curHandle = help.Handle;
+                windowHandle = new WindowInteropHelper(this).Handle;
+
                 //处理alt+tab可以看见本程序
-                int exStyle = User.GetWindowLong(curHandle, User.GWL_EXSTYLE);
+                int exStyle = User.GetWindowLong(windowHandle, User.GWL_EXSTYLE);
                 exStyle |= (int)Win32.User.WS_EX_TOOLWINDOW;
-                User.SetWindowLong(curHandle, User.GWL_EXSTYLE, exStyle);
+                User.SetWindowLong(windowHandle, User.GWL_EXSTYLE, exStyle);
 
                 //查找任务栏容器
                 var iconParentPtr = (IntPtr)User.FindWindowEx(IntPtr.Zero, IntPtr.Zero, "Shell_TrayWnd", null);
-                iconParentPtr = (IntPtr)User.FindWindowEx(iconParentPtr, IntPtr.Zero, "ReBarWindow32", null);
+                panelHandle = (IntPtr)User.FindWindowEx(iconParentPtr, IntPtr.Zero, "ReBarWindow32", null);
+
                 //获取任务栏宽高
-                var rect = new RECT() { };
-                User.GetWindowRect(iconParentPtr, ref rect);
-                var height = rect.Bottom - rect.Top;
-                var width = rect.Right - rect.Left;
-                var parent = iconParentPtr;
+                var size = GetWindowSize(panelHandle);
                 //嵌入本窗体到任务栏容器
-                User.SetParent(curHandle, parent);
-                User.SetWindowPos(curHandle, (IntPtr)(0), width - 90, 0, 90, height, 0x0040);
+                User.SetParent(windowHandle, panelHandle);
+                User.SetWindowPos(windowHandle, (IntPtr)(0), (int)size.Width - 90, 0, 90, (int)size.Height, 0x0040);
                 //设置任务栏变短一点，给本窗体留出空间显示
-                iconParentPtr = (IntPtr)User.FindWindowEx(iconParentPtr, IntPtr.Zero, "MSTaskSwWClass", null);
-                User.SetWindowPos(iconParentPtr, (IntPtr)(0), 0, 0, width - 90, height, 0x0040);
+                taskBarHandle = (IntPtr)User.FindWindowEx(iconParentPtr, IntPtr.Zero, "MSTaskSwWClass", null);
+                User.SetWindowPos(taskBarHandle, (IntPtr)(0), 0, 0, (int)size.Width - 90, (int)size.Height, 0x0040);
             }
             catch (Exception ex)
             {
                 MessageBox.Show("出错:" + ex.Message);
             }
+
+            Task.Run(CheckUpdateWindowPosition);
+        }
+        /// <summary>
+        /// 检查更新窗体位置
+        /// </summary>
+        public void CheckUpdateWindowPosition()
+        {
+
+            while (true)
+            {
+                Thread.Sleep(500);
+
+                var size = GetWindowSize(panelHandle);
+
+                if (size.Width < size.Height)
+                {
+                    //任务栏为竖着的
+                    User.SetWindowPos(windowHandle, (IntPtr)(0), 0, (int)size.Height - 30, (int)size.Width, 30, 0x0040);
+                    User.SetWindowPos(taskBarHandle, (IntPtr)(0), 0, 0, (int)size.Width, (int)size.Height - 30, 0x0040);
+                }
+                else
+                {
+                    //任务栏为横着的
+                    User.SetWindowPos(windowHandle, (IntPtr)(0), (int)size.Width - 90, 0, 90, (int)size.Height, 0x0040);
+                    User.SetWindowPos(taskBarHandle, (IntPtr)(0), 0, 0, (int)size.Width - 90, (int)size.Height, 0x0040);
+
+                }
+            }
+        }
+        /// <summary>
+        /// 获取窗体大小
+        /// </summary>
+        /// <param name="intPtr"></param>
+        /// <returns></returns>
+        private Size GetWindowSize(IntPtr intPtr)
+        {
+            //获取任务栏宽高
+            var rect = new RECT() { };
+            User.GetWindowRect(panelHandle, ref rect);
+            return new Size { Width = rect.Right - rect.Left, Height = rect.Bottom - rect.Top };
         }
     }
 }
