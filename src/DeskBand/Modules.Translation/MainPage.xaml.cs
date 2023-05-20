c5b054882f9684387323aeab8e8a08e412627989
zzccchen <zzccchen@qq.com>
2023-05-20 16:39:26 +0800

    移除多余按钮

--- src/DeskBand/Modules.Translation/MainPage.xaml.cs
+++ src/DeskBand/Modules.Translation/MainPage.xaml.cs
@@ -1,21 +1,10 @@
 ﻿using CefSharp;
 using CefSharp.Wpf;
-using CefSharp.Wpf.Internals;
-using Modules.Translation.Custom;
 using System;
-using System.Buffers;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Text.Json;
-using System.Text.Unicode;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -30,10 +19,13 @@ namespace Modules.Translation
     public partial class MainPage : Page
     {
         #region Fields
-        WebClient client = new WebClient();
-        #endregion
+
+        private WebClient client = new WebClient();
+
+        #endregion Fields
 
         #region Methods
+
         public MainPage()
         {
             CefSettings _settings = new CefSettings();
@@ -41,14 +33,14 @@ namespace Modules.Translation
             Cef.Initialize(_settings);
             InitializeComponent();
             //支持中文输入，但是ime不能定位到光标位置，但是可以在popup里面输入中文
-            Browser.WpfKeyboardHandler = new WpfKeyboardHandler(Browser);
             //支持中文输入以及ime定位但是在popup里面失效，推测是popup没有实体句柄之类的
             //Browser.WpfKeyboardHandler = new WpfImeKeyboardHandler(Browser);
         }
 
-        #endregion
+        #endregion Methods
 
         #region Events
+
         /// <summary>
         /// 检测ctrl+v自动翻译
         /// </summary>
@@ -61,6 +53,7 @@ namespace Modules.Translation
             //    CommandBindingTranslation_Executed(null, null);
             //}
         }
+
         /// <summary>
         /// 退出
         /// </summary>
@@ -70,6 +63,7 @@ namespace Modules.Translation
         {
             Application.Current.Shutdown();
         }
+
         /// <summary>
         /// 跳转项目地址
         /// </summary>
@@ -84,135 +78,11 @@ namespace Modules.Translation
             };
             Process.Start(psi);
         }
-        /// <summary>
-        /// 鼠标离开的时候隐藏popup
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void popBro_MouseLeave(object sender, MouseEventArgs e)
-        {
-            if (cheHide.IsChecked.Value == true)
-                popBro.SetCurrentValue(Popup.IsOpenProperty, false);
-        }
-        #endregion
 
-        #region Commands
-        /// <summary>
-        /// 翻译命令
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CommandBindingTranslation_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(txtSource.Text))
-                return;
-            Task.Run(() =>
-            {
-                string msg = string.Empty;
-                string lan = string.Empty;
-                Dispatcher.Invoke(() =>
-                {
-                    msg = txtSource.Text;
-                    lan = togLanguage.IsChecked.Value ? "en" : "zh-Hans";
-                });
-                try
-                {
-                    //zh-Hans
-                    var url = "https://cn.bing.com/ttranslatev3?isVertical=1&&IG=FA5F953F30714718B7F4C1DB9C9EFFBC&IID=translator.5024.1";
-                    var data = Encoding.UTF8.GetBytes($"&fromLang=auto-detect&text={msg}&to={lan}");
-                    client.Headers.Add("Content-type", "application/x-www-form-urlencoded");
-                    var result = client.UploadData(url, data);
-                    var json = Encoding.UTF8.GetString(result);
-                    var temp = JsonDocument.Parse(json);
-                    msg = temp.RootElement.EnumerateArray().First().GetProperty("translations").EnumerateArray().First().GetProperty("text").ToString();
-                }
-                catch (Exception ex)
-                {
-                    msg = ex.Message;
-                }
+        #endregion Events
 
-                Dispatcher.Invoke(() =>
-                {
-                    txtResult.Text = msg;
-                    txtResult.SelectAll();
-                    txtResult.Focus();
+        private static SolidColorBrush polygonBrush = new SolidColorBrush(Color.FromArgb(128, 255, 255, 255));
 
-                });
-            });
-        }
-        /// <summary>
-        /// 格式化命令
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CommandBindingFormat_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(txtSource.Text))
-                return;
-            try
-            {
-                var buffer = new ArrayBufferWriter<byte>();
-                var options = new JsonWriterOptions
-                {
-                    Indented = true,
-                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
-                };
-
-                using var json = new Utf8JsonWriter(buffer, options);
-                JsonDocument.Parse(txtSource.Text).WriteTo(json);
-                json.Flush();
-                txtResult.Text = Encoding.UTF8.GetString(buffer.WrittenSpan.ToArray());
-                txtResult.SelectAll();
-                txtResult.Focus();
-            }
-            catch (Exception ex)
-            {
-                txtResult.Text = ex.Message;
-            }
-        }
-        /// <summary>
-        /// 选择所有文本命令
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CommandBindingSelectAll_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            txtSource.SelectAll();
-        }
-        /// <summary>
-        /// Run命令
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CommandBindingGo_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(txtSource.Text))
-                return;
-            try
-            {
-                var psi = new ProcessStartInfo
-                {
-                    FileName = txtSource.Text,
-                    UseShellExecute = true
-                };
-                Process.Start(psi);
-            }
-            catch (Exception)
-            {
-            }
-        }
-        /// <summary>
-        /// 浏览命令
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CommandBindingBrowser_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            Browser.Address = txtAddress.Text;
-        }
-        #endregion
-
-        static SolidColorBrush polygonBrush = new SolidColorBrush(Color.FromArgb(128, 255, 255, 255));
         /// <summary>
         /// 按钮点击开始涟漪动画
         /// </summary>
@@ -235,7 +105,5 @@ namespace Modules.Translation
             //开始透明度消失动画
             path.BeginAnimation(Path.OpacityProperty, new DoubleAnimation(1, 0, new Duration(TimeSpan.FromSeconds(time))));
         }
-
-      
     }
 }
