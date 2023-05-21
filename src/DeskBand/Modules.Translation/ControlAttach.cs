4c8cfc45d1505c903197e65e04657668bec11705
zzccchen <zzccchen@qq.com>
2023-05-21 17:56:05 +0800

    暂存

--- src/DeskBand/Modules.Translation/ControlAttach.cs
+++ src/DeskBand/Modules.Translation/ControlAttach.cs
@@ -1,71 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Media;
+﻿using System.Windows;
 using System.Windows.Data;
-using System.Windows.Shapes;
+using System.Windows.Media;
 
 namespace Modules.Translation
 {
     /// <summary>
-    /// Attach  property for control
+    /// Attach property for control
     /// </summary>
     public class ControlAttach
     {
         #region 按钮附加图标
-        public static UIElement GetIcon(DependencyObject obj)
-        {
-            return (UIElement)obj.GetValue(IconProperty);
-        }
 
-        public static void SetIcon(DependencyObject obj, UIElement value)
-        {
-            obj.SetValue(IconProperty, value);
-        }
         public static readonly DependencyProperty IconProperty =
             DependencyProperty.RegisterAttached("Icon", typeof(UIElement), typeof(ControlAttach), new FrameworkPropertyMetadata()
             {
-                DefaultValue=null,
+                DefaultValue = null,
                 BindsTwoWayByDefault = true,
-                DefaultUpdateSourceTrigger=UpdateSourceTrigger.PropertyChanged
+                DefaultUpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
             });
 
-        #endregion
+        #endregion 按钮附加图标
 
         #region 鼠标悬停时显示的内容
-        public static UIElement GetMouseOverContent(DependencyObject obj)
-        {
-            return (UIElement)obj.GetValue(MouseOverContentProperty);
-        }
 
-        public static void SetMouseOverContent(DependencyObject obj, UIElement value)
-        {
-            obj.SetValue(MouseOverContentProperty, value);
-        }
-        
-        // Using a DependencyProperty as the backing store for MouseOverContent.  This enables animation, styling, binding, etc...fdfsd
+        // Using a DependencyProperty as the backing store for MouseOverContent. This enables
+        // animation, styling, binding, etc...fdfsd
         public static readonly DependencyProperty MouseOverContentProperty =
-            DependencyProperty.RegisterAttached("MouseOverContent", typeof(UIElement), typeof(ControlAttach),new FrameworkPropertyMetadata() { DefaultValue=null,BindsTwoWayByDefault=true});
-        #endregion
+            DependencyProperty.RegisterAttached("MouseOverContent", typeof(UIElement), typeof(ControlAttach), new FrameworkPropertyMetadata() { DefaultValue = null, BindsTwoWayByDefault = true });
 
-
-        public static Brush GetHeaderBackground(DependencyObject obj)
-        {
-            return (Brush)obj.GetValue(HeaderBackgroundProperty);
-        }
-
-        public static void SetHeaderBackground(DependencyObject obj, Brush value)
-        {
-            obj.SetValue(HeaderBackgroundProperty, value);
-        }
+        #endregion 鼠标悬停时显示的内容
 
         /// <summary>
         /// 控件头部背景颜色
         /// </summary>
         public static readonly DependencyProperty HeaderBackgroundProperty =
             DependencyProperty.RegisterAttached("HeaderBackground", typeof(Brush), typeof(ControlAttach), new PropertyMetadata(null));
-
 
         public static CornerRadius GetCornerRadius(DependencyObject obj)
         {
@@ -82,8 +51,5 @@ namespace Modules.Translation
         /// </summary>
         public static readonly DependencyProperty CornerRadiusProperty =
             DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(ControlAttach), new PropertyMetadata(default(CornerRadius)));
-
-
-
     }
 }
